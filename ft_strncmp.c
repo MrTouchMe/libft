32f78eb4c92d302f9de7dcb03d8b482bc165c3ab
MrTouchMe <135585161+MrTouchMe@users.noreply.github.com>
2023-10-10 10:20:08 +0200

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dgiurgev <dgiurgev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 21:11:13 by dgiurgev          #+#    #+#             */
-/*   Updated: 2023/10/09 08:17:02 by dgiurgev         ###   ########.fr       */
+/*   Updated: 2023/10/10 10:18:54 by dgiurgev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
-	{
-		if ((size_t) i == n - 1)
-			break ;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i <= n)
 		i++;
-	}
-	return (s2 - s1);
+	return (s1[i] - s2[i]);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,17 +6,19 @@
 /*   By: dgiurgev <dgiurgev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 00:11:05 by dgiurgev          #+#    #+#             */
-/*   Updated: 2023/10/10 06:22:56 by dgiurgev         ###   ########.fr       */
+/*   Updated: 2023/10/10 08:41:48 by dgiurgev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "libft.h"
+#include <stdio.h> // Include standard I/O for printing
 
 char	*ft_strrchr(const char*s, int c)
 {
 	int	len;
 
-	len = ft_strlen (s);
+	len = ft_strlen(s);
 	while (len >= 0)
 	{
 		if (*(s + len) == c)
