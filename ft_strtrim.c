eba93974f190f6d93f633cea3f4108f101acd56c
MrTouchMe <135585161+MrTouchMe@users.noreply.github.com>
2023-10-14 05:57:30 +0200

    Add files via upload

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dgiurgev <dgiurgev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/13 02:04:11 by dgiurgev          #+#    #+#             */
-/*   Updated: 2023/10/14 05:25:09 by dgiurgev         ###   ########.fr       */
+/*   Created: 2023/10/14 05:34:21 by dgiurgev          #+#    #+#             */
+/*   Updated: 2023/10/14 05:34:49 by dgiurgev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	total_size;
-	void	*ptr;
-
-	if (count > 2147483647 || size > 2147483647)
-		return (NULL);
-	total_size = count * size;
-	ptr = malloc(total_size);
-	if (ptr == NULL)
-		return (NULL);
-	ft_bzero(ptr, total_size);
-	return (ptr);
+	
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dgiurgev <dgiurgev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 07:18:47 by dgiurgev          #+#    #+#             */
-/*   Updated: 2023/10/14 01:08:40 by dgiurgev         ###   ########.fr       */
+/*   Updated: 2023/10/14 05:44:13 by dgiurgev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,4 +40,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
 #endif
