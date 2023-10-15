d09df5d17e1e0c2bd29f9aa4eb1ed6b58f8386af
MrTouchMe <135585161+MrTouchMe@users.noreply.github.com>
2023-10-16 01:31:01 +0200

    Add files via upload

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: dgiurgev <dgiurgev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 05:35:20 by dgiurgev          #+#    #+#             */
-/*   Updated: 2023/10/14 05:58:04 by dgiurgev         ###   ########.fr       */
+/*   Updated: 2023/10/16 01:30:15 by dgiurgev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,39 @@
 
 char	*ft_itoa(int n)
 {
-	
+	int			sign;
+	int			numdigits;
+	long int	lin;
+
+	sign = 1;
+	numdigits = 0;
+	lin = n;
+	if (lin < 0)
+	{
+		sign = -1;
+		lin = -lin;
+		numdigits++;
+	}
+	long int temp = lin;
+	while (temp >= 10)
+	{
+		numdigits++;
+		temp /= 10;
+	}
+	numdigits++;
+	char *result = (char *)malloc((numdigits + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	result[numdigits] = '\0';
+	int i = numdigits - 1;
+	while (lin >= 10)
+	{
+		result[i] = (lin % 10) + '0';
+		lin /= 10;
+		i--;
+	}
+	result[i] = lin + '0';
+	if (sign == -1)
+		result[0] = '-';
+	return (result);
 }
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dgiurgev <dgiurgev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 05:33:28 by dgiurgev          #+#    #+#             */
-/*   Updated: 2023/10/14 05:58:12 by dgiurgev         ###   ########.fr       */
+/*   Updated: 2023/10/15 18:35:48 by dgiurgev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	
+	size_t	i;
+	size_t	j;
+	char	*result;
+
+	i = 0;
+	j = 0;
+	result = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (result == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		result[i] = s2[j];
+		i++;
+		j++;
+	}
+	result[i] = '\0';
+	return (result);
 }
